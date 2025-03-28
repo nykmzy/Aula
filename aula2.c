327f6de641be00167502dfb2610afc44cf4eb785
Nataly Kamizi <natalykamizi00@gmail.com>
2025-03-28 22:11:50 +0000

    aula operadores mat

---
+++ aula2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main () {
+    int numero1 = 10, numero2, resultado;
+
+resultado = 10;
+printf("Resultado: %d\n", resultado);
+
+resultado += 20;
+printf("Resultado: %d\n", resultado);
+
+resultado -= numero1;
+printf("Resultado: %d\n", resultado);
+
+resultado *= 5;
+printf("Resultado: %d\n", resultado);
+
+resultado /= 2;
+printf("Resultado: %d\n", resultado);
+
+return 0;
+}
---
+++ aula3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+int numero1 = 1, resultado;
+
+printf("Antes incremento %d\n", numero1);
+resultado = numero1++;
+printf("apos incremento %d\n", numero1);
+
+
+}
